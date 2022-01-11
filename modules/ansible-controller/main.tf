2e022cdc2f204009a204ca9bdf7c1ed71081f98f
cheretbe <a.a.orlov@gmail.com>
2022-01-11 23:25:57 +0200

    modules/ansible-controller: check out common Ansible repo

--- modules/ansible-controller/main.tf
+++ modules/ansible-controller/main.tf
@@ -14,4 +14,26 @@ resource "null_resource" "ansible-controller" {
   provisioner "remote-exec" {
     inline = ["/usr/bin/curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/master/setup_venv.py?flush_cache=True | /usr/bin/python3 - ansible --batch-mode --requirement /home/${var.connection.user}/requirements.txt"]
   }
+
+  provisioner "remote-exec" {
+    inline = ["mkdir -p /tmp/vagrant-provision/"]
+  }
+
+  provisioner "file" {
+    source = "${path.module}/controller_provision.yml"
+    destination = "/tmp/vagrant-provision/controller_provision.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      join("", [
+        "/home/${var.connection.user}/.cache/venv/ansible/bin/ansible-playbook ",
+        " -i localhost, -c local /tmp/vagrant-provision/controller_provision.yml"
+      ])
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = ["rm -rf /tmp/vagrant-provision/"]
+  }
 }
